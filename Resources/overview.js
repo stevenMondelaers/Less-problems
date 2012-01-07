5754ee591e952bfe4b9cd5242518acc759fc95f2
Steven Mondelaers <steven.mondelaers@gmail.com>
2012-01-07 23:12:24 +0100

    Feedback tijdens laden van overview

--- Resources/overview.js
+++ Resources/overview.js
@@ -1,12 +1,20 @@
 
 var window_overzicht = Ti.UI.createWindow({
-  backgroundColor : "#fff",
+  backgroundColor : "#333",
   title: 'Overzicht problemen'
   //navBarHidden:true,
   //fullscreen: true
 });
 
 var view_overzicht = Ti.UI.createView();
+var view_overzichtLoading = Ti.UI.createView();
+
+var lbl_loading = Ti.UI.createLabel({
+	text:'Loading problems'
+});
+
+view_overzichtLoading.add(lbl_loading);
+view_overzicht.add(view_overzichtLoading);
 
 var tableview_overzicht = Titanium.UI.createTableView();
 
@@ -14,14 +22,21 @@ var xhr = Titanium.Network.createHTTPClient();
 
 xhr.onload = function()
 {
+	tableview_overzicht.visible = false;
+	view_overzichtLoading.visible = true;
+	
 	var doc = this.responseXML.documentElement;		
 	var elements = doc.getElementsByTagName("title");
+	
+	tableview_overzicht.visible = true;
+	view_overzichtLoading.visible = false;
 		
 	for(i = 0 ; i < elements.length ; i++)
 	{
 		var title = elements.item(i);
 		
 		tableview_overzicht.appendRow(Ti.UI.createTableViewRow({hasChild: true, title: '' + title.text}));
+		
 	}
 };
 
--- build/android/bin/assets/Resources/overview.js
+++ build/android/bin/assets/Resources/overview.js
@@ -1,12 +1,29 @@
 
 var window_overzicht = Ti.UI.createWindow({
-  backgroundColor : "#fff",
+  backgroundColor : "#333",
   title: 'Overzicht problemen'
   //navBarHidden:true,
   //fullscreen: true
 });
 
 var view_overzicht = Ti.UI.createView();
+var view_overzichtLoading = Ti.UI.createView({
+	//visible:false
+});
+
+var lbl_loading = Ti.UI.createLabel({
+	text:'Loading problems'
+});
+
+var img_loading = Ti.UI.createImageView({
+	image:'img/KS_nav_ui.png',
+	height:46,
+	width:43
+})
+
+view_overzichtLoading.add(lbl_loading);
+view_overzichtLoading.add(img_loading);
+view_overzicht.add(view_overzichtLoading);
 
 var tableview_overzicht = Titanium.UI.createTableView();
 
@@ -16,12 +33,15 @@ xhr.onload = function()
 {
 	var doc = this.responseXML.documentElement;		
 	var elements = doc.getElementsByTagName("title");
+	
+	view_overzichtLoading.visible = false;
 		
 	for(i = 0 ; i < elements.length ; i++)
 	{
 		var title = elements.item(i);
 		
 		tableview_overzicht.appendRow(Ti.UI.createTableViewRow({hasChild: true, title: '' + title.text}));
+		
 	}
 };
 
@@ -30,82 +50,3 @@ xhr.send();
 
 view_overzicht.add(tableview_overzicht);
 window_overzicht.add(view_overzicht);
-
-
-
-/*
-var tabelData = [];
-var tabel = Ti.UI.createTableView({
-	data : tabelData,
-	height:'100%',
-	width:'100%'
-});
-
-var updateKnop = Ti.UI.createButton({
-	title : 'Update overview'
-})
-
-updateKnop.addEventListener('click', loadOverview);
-overzichtView.add(updateKnop);
-
-function loadOverview(){
-	
-	tabelData = [];
-		
-var xhr = Titanium.Network.createHTTPClient();
-
-
-
-xhr.onload = function(e){
-	var doc = this.responseXML.documentElement;
-	
-
-    var elements = doc.getElementsByTagName("vid");
-
-	for(i = 0 ; i < elements.length ; i++){
-		
-		try{
-		var t = i;
-		var titel = doc.getElementsByTagName("title").item(i).text;
-		var lokaal = doc.getElementsByTagName("value").item((t*2+1)).text;
-		
-		var lblLokaal = Ti.UI.createLabel({
-			text : lokaal,
-			right:5,
-			top: -20
-		});
-		
-		var lblTitel = Ti.UI.createLabel({
-			text : titel,
-			left:5
-		});
-		
-		var rij = Ti.UI.createTableViewRow({
-			layout:'vertical',
-			height:25
-		});
-		
-		rij.add(lblTitel);
-		rij.add(lblLokaal);
-		tabelData.push(rij);
-	
-		}catch (ex){
-		
-		}
-						
-	}
-	tabel.setData(tabelData);
-	overzichtView.add(tabel);
-	tabel.show();
-	
-}
-
-xhr.open('GET', 'http://less-problems.webatu.com/api/views/problems.xml');
-xhr.send();
-}
-
-loadOverview();
-
-window_overzicht.add(overzichtView);
-overzichtView.add(label1);
-*/
